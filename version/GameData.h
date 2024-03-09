b0336c37cc50d304b5eb55b327d1bedd18514334
Meatfleet <darrow.tuomas@gmail.com>
2024-03-09 22:36:21 +0700

    Update GameData.h

--- version/GameData.h
+++ version/GameData.h
@@ -2,28 +2,28 @@
 class GameData
 {
 	public:
-		bool	InfAmmo = false;
-		bool	InfAmmoLegit = false;
+		bool	InfAmmo = true;
+		bool	InfAmmoLegit = true;
 		bool	InfGrenades = false;
-		bool	InfGrenadesLegit = false;
+		bool	InfGrenadesLegit = true;
 		bool	InfSyringes = false;
-		bool	InfSyringesLegit = false;
-		bool	InfStratagems = false;
+		bool	InfSyringesLegit = true;
+		bool	InfStratagems = true;
 		bool	InfMissionTime = false;
-		bool	InfStamina = false;
-		bool	InfHealth = false;
-		bool	NoReload = false;
+		bool	InfStamina = true;
+		bool	InfHealth = true;
+		bool	NoReload = true;
 		bool	MaxResources = false;
 		bool	Add5Resources = false;
 		bool	Recoil = false;
-		bool	InfBackpack = false;
-		bool	InfSpecWeapon = false;
+		bool	InfBackpack = true;
+		bool	InfSpecWeapon = true;
 		bool	NoCannonOverheat = false;
 		bool	NoStasTurretOverHeat = false;
 		bool	JetpackNoCD = false;
 		bool	ShieldNoCD = false;
 		bool	InstantRailGun = false;
-		bool	ShowAllMapIcons = false;
+		bool	ShowAllMapIcons = true;
 		bool	AllStratagems = false;
 		bool	AllEquipment = false;
 		bool	AllArmor = false;
